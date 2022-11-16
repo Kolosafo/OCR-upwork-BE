01141824f64923c2a02f5d8a5f8b640f0994520b
unknown <daudakolo16@gmail.com>
2022-11-16 16:44:07 +0100

    Docker error loads

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 set -e
-RUN_PORT=${PORT:-8000}
 
 /usr/local/bin/gunicorn ocr_backend.wsgi:application --preload --bind 0.0.0.0:8000 --workers=4 -t 300
-
-echo "done"
-echo "$@"
-exec "$@"
